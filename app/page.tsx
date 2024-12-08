236be331083f37e5de37a7ec9a7a87e6b5d63b5e
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-09 00:52:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
-import SMSOTP from '@/components/magic/auth/SMSOTP';
-import SendTransaction from '@/components/magic/SendTransactionCard';
-import { UserInfo } from '@/components/magic/UserInfo';
-// import { PlayPage } from '@/components/PlayPage/PlayPage';
+// import SMSOTP from '@/components/magic/auth/SMSOTP';
+// import SendTransaction from '@/components/magic/SendTransactionCard';
+// import { UserInfo } from '@/components/magic/UserInfo';
+import { PlayPage } from '@/components/PlayPage/PlayPage';
 
 export default function Home() {
 	return (
 		<div>
-			<SMSOTP />
-			<UserInfo />
-			<SendTransaction />
+			<PlayPage />
 		</div>
 	);
 }
