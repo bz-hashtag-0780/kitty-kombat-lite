00bcc67e77b3aee2e974f020016aeaa7c69a99ac
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-12 12:18:34 +0100

    Update Footer.tsx

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
 import { Gamepad2, Gift, ShoppingCart } from 'lucide-react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -10,44 +9,38 @@ export const Footer = () => {
 
 	// Function to determine active styles
 	const getActiveStyle = (path: string) => {
-		return pathname === path && 'text-yellow-500 text-shadow';
+		return pathname === path ? 'text-yellow-500 text-shadow' : '';
 	};
 
 	return (
 		<div className="border-t border-gray-800 bg-gray-900 text-white">
 			<div className="flex justify-around p-4">
-				<Link href="/shop">
-					<Button
-						variant="ghost"
-						className={`flex-col gap-1 h-auto py-2 ${getActiveStyle(
-							'/shop'
-						)}`}
-					>
-						<ShoppingCart className="w-5 h-5" />
-						<span className="text-xs">Shop</span>
-					</Button>
+				<Link
+					href="/shop"
+					className={`flex flex-col items-center gap-2 h-16 w-full text-center ${getActiveStyle(
+						'/shop'
+					)}`}
+				>
+					<ShoppingCart className="w-6 h-6" />
+					<span className="text-xs">Shop</span>
 				</Link>
-				<Link href="/play">
-					<Button
-						variant="ghost"
-						className={`flex-col gap-1 h-auto py-2 ${getActiveStyle(
-							'/'
-						)} ${getActiveStyle('/play')}`}
-					>
-						<Gamepad2 className="w-5 h-5" />
-						<span className="text-xs">Play</span>
-					</Button>
+				<Link
+					href="/play"
+					className={`flex flex-col items-center gap-2 h-16 w-full text-center ${getActiveStyle(
+						'/'
+					)} ${getActiveStyle('/play')}`}
+				>
+					<Gamepad2 className="w-6 h-6" />
+					<span className="text-xs">Play</span>
 				</Link>
-				<Link href="/earn">
-					<Button
-						variant="ghost"
-						className={`flex-col gap-1 h-auto py-2 ${getActiveStyle(
-							'/earn'
-						)}`}
-					>
-						<Gift className="w-5 h-5" />
-						<span className="text-xs">Earn</span>
-					</Button>
+				<Link
+					href="/earn"
+					className={`flex flex-col items-center gap-2 h-16 w-full text-center ${getActiveStyle(
+						'/earn'
+					)}`}
+				>
+					<Gift className="w-6 h-6" />
+					<span className="text-xs">Earn</span>
 				</Link>
 			</div>
 		</div>
